703ead7f97ef75fa3e2c144ee01f384efad587c8
DongwooYi <108648466+DongwooYi@users.noreply.github.com>
2022-10-24 13:51:54 +0900

    Squashed commit of the following:
    commit d647ad78f14ebdf2254e4963db43471c95b9daea
    Author: DongwooYi <108648466+DongwooYi@users.noreply.github.com>
    Date:   Mon Oct 24 13:50:26 2022 +0900
    
        로그인 기타등등
    
        로그인 기타등등
    
    commit 14d5757d4caa47b7801e01e33ec648fc7cbd58b7
    Author: DongwooYi <108648466+DongwooYi@users.noreply.github.com>
    Date:   Mon Oct 24 10:23:11 2022 +0900
    
        카메라
    
        카메라
    
    commit 91e54c0e73e5adf7ceabcb3e110218ee1bfe8ced
    Author: DongwooYi <108648466+DongwooYi@users.noreply.github.com>
    Date:   Sun Oct 23 01:25:38 2022 +0900
    
        수정
    
        수정
    
    commit 0afd515236b182f1b29dfba9b9c3f409ab759c49
    Author: DongwooYi <108648466+DongwooYi@users.noreply.github.com>
    Date:   Sat Oct 22 23:56:30 2022 +0900
    
        11
    
    commit 63df8c81afaa7c0e12fdebb3e2125318ab85294d
    Merge: a173a55 a0a488b
    Author: DongwooYi <108648466+DongwooYi@users.noreply.github.com>
    Date:   Sat Oct 22 23:55:03 2022 +0900
    
        Merge branch 'main' into FinalProject_YDW
    
    commit a173a55b11f1d910be302d396db56e4b424ffb5a
    Author: DongwooYi <108648466+DongwooYi@users.noreply.github.com>
    Date:   Sat Oct 22 23:54:16 2022 +0900
    
        1
    
        1
    
    commit e68c09744b9dbe5791d47ae168b5a77093f435a3
    Author: DongwooYi <108648466+DongwooYi@users.noreply.github.com>
    Date:   Sat Oct 22 23:46:36 2022 +0900
    
        수정
    
        수정

--- Assets/YDW/Scripts_YDW/PhoneCamera.cs
+++ Assets/YDW/Scripts_YDW/PhoneCamera.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
+using System.IO;
 public class PhoneCamera : MonoBehaviour
 {
     bool cameraAvailable;
@@ -13,31 +13,32 @@ public class PhoneCamera : MonoBehaviour
     public AspectRatioFitter fit;
     private void Start()
     {
-      /*  defaultBackground = background.texture;
-        WebCamDevice[] devices = WebCamTexture.devices;
-    if(devices.Length ==0)
-        {
-            Debug.Log("카메라 없음");
-            cameraAvailable = false;
-            return;       
+     
+            /*  defaultBackground = background.texture;
+              WebCamDevice[] devices = WebCamTexture.devices;
+          if(devices.Length ==0)
+              {
+                  Debug.Log("카메라 없음");
+                  cameraAvailable = false;
+                  return;       
+              }
+              for (int i = 0; i < devices.Length; i++)
+              {
+                  if(!devices[i].isFrontFacing)
+                  {
+                      backCam = new WebCamTexture(devices[i].name, Screen.width, Screen.height);
+                  }
+              }
+
+              if(backCam== null)
+              {
+                  Debug.Log("후면 카메라 찾을 수없음");
+                  return;
+              }
+              backCam.Play();
+              background.texture = backCam;
+              cameraAvailable = true; */
         }
-        for (int i = 0; i < devices.Length; i++)
-        {
-            if(!devices[i].isFrontFacing)
-            {
-                backCam = new WebCamTexture(devices[i].name, Screen.width, Screen.height);
-            }
-        }
-        
-        if(backCam== null)
-        {
-            Debug.Log("후면 카메라 찾을 수없음");
-            return;
-        }
-        backCam.Play();
-        background.texture = backCam;
-        cameraAvailable = true; */
-    }
 
     private void Update()
     {
@@ -47,8 +48,8 @@ public class PhoneCamera : MonoBehaviour
         }
         float ratio = (float)backCam.width / (float)backCam.height;
         fit.aspectRatio = ratio;
-        float scaleY = backCam.videoVerticallyMirrored ? -1f : 1f;
-        background.rectTransform.localScale = new Vector3(1f, scaleY, 1f);
+        float scaleY = backCam.videoVerticallyMirrored ? -10f : 10f;
+        background.rectTransform.localScale = new Vector3(10f, scaleY, 10f);
         int orient = -backCam.videoRotationAngle;
         background.rectTransform.localEulerAngles = new Vector3(0, 0, orient);
     }
@@ -79,5 +80,31 @@ public class PhoneCamera : MonoBehaviour
         backCam.Play();
         background.texture = backCam;
         cameraAvailable = true;
+    }
+    
+    public void CameraOff()
+    {
+        StartCoroutine(TakeSnap());
+        backCam.Stop();
+        cameraAvailable = false;
+        
+    }
+    IEnumerator TakeSnap()
+    {
+        //print("111111111111");
+        yield return new WaitForEndOfFrame();
+        int width = backCam.width;
+        int height = backCam.height;
+        Texture2D snap = new Texture2D(width, height, TextureFormat.RGB24,false);
+
+        // 스크린 샷 정보를 텍스쳐 형식으로 
+        snap.ReadPixels(new Rect(0, 0, width, height), 0, 0);
+        snap.Apply();
+        //snap.SetPixels(backCam.GetPixels());
+        
+        byte[] bytes = snap.EncodeToPNG();
+        Object.Destroy(snap);
+        // 테스트용
+         File.WriteAllBytes(Application.dataPath + "/../SavedScreen.png", bytes);
     }
 }

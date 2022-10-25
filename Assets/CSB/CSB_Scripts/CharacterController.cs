cfcdbc54f4c23b0996e20f62c352d4cccaba330f
sbeencho <glitterbeen99@gmail.com>
2022-10-25 21:12:06 +0900

    메인 월드 제작2
    메인 월드 제작2

--- Assets/CSB/CSB_Scripts/CharacterController.cs
+++ Assets/CSB/CSB_Scripts/CharacterController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 
@@ -8,10 +9,12 @@ using UnityEngine.SceneManagement;
 // 조이스틱 입력 값에 따라 상하좌우 이동
 public class CharacterController : MonoBehaviour
 {
+    public GameObject spawnPosition;
+    
     public float speed = 5f;
+    public bool enterTheWorld = false;
 
     Rigidbody rb;
-    public GameObject spawnPosition;
 
     private void Awake()
     {
@@ -28,7 +31,6 @@ public class CharacterController : MonoBehaviour
 
 
     }
-    public bool enterTheWorld = false;
 
 
     void Update()
@@ -50,6 +52,7 @@ public class CharacterController : MonoBehaviour
         }
     }
 
+   // 플레이어 이동
     public void Move(Vector2 inputDir)
     {
         // 이동 방향키 입력 값 가져오기
@@ -64,7 +67,24 @@ public class CharacterController : MonoBehaviour
 
             // 이동
             transform.position += moveDir * Time.deltaTime * speed;
+        }
+    }
 
+
+    // 진행 중인 챌린지 목록
+    public void IngChallenge()
+    {
+        GameObject obj = GameObject.Find("IngChallenge");   // 물체 찾기
+        // 빈 게임오브젝트에 text 붙여서
+
+        // 만약 물체와의 거리가 1보다 작으면
+        if(Vector3.Distance(transform.position, obj.transform.position) < 1f)
+        {
+            // <챌린지 참가> UI 가 뜸, setActive
+
+            // 만약 물체와의 거리가 0.5 보다 작으면 진행 중인 챌린지 목록(UI)이 뜸
+            //if()
+            // 하나를 선택하면 챌린지 월드로 입장
         }
 
     }

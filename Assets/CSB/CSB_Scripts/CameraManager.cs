197b8413f11778b0fd6a5a10ac501a1b65987df8
sbeencho <glitterbeen99@gmail.com>
2022-10-27 20:02:50 +0900

    카메라 움직임

--- Assets/CSB/CSB_Scripts/CameraManager.cs
+++ Assets/CSB/CSB_Scripts/CameraManager.cs
@@ -1,15 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.EventSystems;
 
-// 두 손가락으로 화면 줌인, 줌아웃 (카메라의 거리..?)
-// 한 손가락으로 카메라 이동
-// UI 건들면 리턴
+// touchCount == 0 -> stop
+//  if) follow Player
+// touchCount == 1 -> Camera Move
+//  if) Object -> follow Object
+// touchCount == 2 -> ZumInOut
+// 물건 배치할 때 위에서 아래로
+
+// UI touch -> return
 
 public class CameraManager : MonoBehaviour
 {
     /* 카메라 이동 관련 */
+    public GameObject player;   // 플레이어
     public float camMoveSpeed = 0.5f;   // 카메라 이동 속력
 
     private Vector2 currentPos, prePos; // 손가락 포지션
@@ -21,12 +28,13 @@ public class CameraManager : MonoBehaviour
 
     void Start()
     {
-   
-        
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
     void Update()
     {
+        if (SceneManager.GetActiveScene().name == "CSB_MyProfile") return;
+
         if (Input.touchCount > 0)
         {
             if (!EventSystem.current.IsPointerOverGameObject(Input.GetTouch(0).fingerId) || !EventSystem.current.IsPointerOverGameObject(Input.GetTouch(1).fingerId))  // 만약 UI 위 아니라면
@@ -35,8 +43,29 @@ public class CameraManager : MonoBehaviour
                 GetTouchZoomInOut();
             }
         }
+        else
+        {
+            // 가만히 있음
+            dir = player.transform.position - Camera.main.transform.position;
+            Camera.main.transform.position += dir * Time.deltaTime * 0 * 0.8f;  // 속력 0
+        }
 
     }
+
+    Vector3 dir;
+    
+
+/*    Vector3 dir;
+
+    // 기본 - 플레이어 따라감
+    private void FollowPlayer()
+    {
+
+        dir = player.transform.position - Camera.main.transform.position;   // 방향
+        Vector3 moveVector = new Vector3(dir.x * camMoveSpeed * Time.deltaTime, dir.y * camMoveSpeed * Time.deltaTime, dir.z * camMoveSpeed * Time.deltaTime);
+        Camera.main.transform.Translate(moveVector);
+
+    }*/
 
     // 화면 드래그(카메라 이동) 함수
     // 한 손가락
@@ -46,6 +75,8 @@ public class CameraManager : MonoBehaviour
 
         if(Input.touchCount == 1)
         {
+            // 만약 오브젝트를 잡았다면 오브젝트를 따라감
+
             Touch touch = Input.GetTouch(0);
 
             if(touch.phase == TouchPhase.Began)

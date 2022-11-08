3edddee4d7c586bdde77a4bfef2f00ecca822467
sbeencho <glitterbeen99@gmail.com>
2022-11-08 14:36:21 +0900

    도서 API - 정보 받아오는 것까지 함
    도서 API - 정보 받아오는 것까지 함

---
+++ Assets/CSB_Alpha/SB_Scripts/APIRequester.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+// 책 제목 입력 받으면 Request url에 title 넣어서 requests
+[Serializable]
+public class BookInfo   // 받아오는 책정보
+{
+    public string title;    // 제목
+    public string contents; // 설명
+    public string image;  // 썸네일 이미지 url
+    public string isbn;
+    public string[] authors;
+    public string publisher;
+    public string[] translators;
+}
+
+public class APIRequester : MonoBehaviour
+{
+
+    public Action<DownloadHandler> onComplete;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
--- Assets/CSB_Alpha/SB_Scripts/WorldManager2D.cs
+++ Assets/CSB_Alpha/SB_Scripts/WorldManager2D.cs
@@ -1,14 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 using UnityEngine.UI;
 
-// 챌린지 생성 버튼 관리
+// 책 검색, 책 등록 관리
+
 public class WorldManager2D : MonoBehaviour
 {
     public InputField inputBookTitleName;
-    public InputField inputMaxPeople;
-    public Button btnMakeClub;
+    public Button btnSearch;
+
+    public APIManager manager;
 
     void Start()
     {
@@ -25,21 +28,34 @@ public class WorldManager2D : MonoBehaviour
         
     }
 
-    /* 모임 생성 버튼 관련 */
-    public void MakeClub()
-    {
-
-    }
 
     void OnValueChanged(string s)
     {
-        btnMakeClub.interactable = s.Length > 0;
+        btnSearch.interactable = s.Length > 0;  // 검색 버튼 활성화
     }
+
+    // 검색 버튼 관련
+    public void OnClickSearchBook()
+    {
+        // 검색 버튼을 클릭하면 
+        APIRequester requester = new APIRequester();
+
+        requester.onComplete = OnCompleteSearchBook;
+
+        manager.SendRequest(requester);
+    }
+
+    // 도서 검색 결과 출력
+    public void OnCompleteSearchBook(DownloadHandler handler)
+    {
+        BookInfo info = JsonUtility.FromJson<BookInfo>(handler.text);
+        print("도서 정보 출력");
+    }
+
 
     void OnEndEdit(string s)
     {
         print("OnEndEdit : " + s);
-
     }
 
 }

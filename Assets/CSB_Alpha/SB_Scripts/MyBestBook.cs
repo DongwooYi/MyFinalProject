89515d7f5943e0a66aa6facaf75f33f9c496d711
sbeencho <glitterbeen99@gmail.com>
2022-11-21 09:09:44 +0900

    오류가 아직 해결되지 않았습니다아

--- Assets/CSB_Alpha/SB_Scripts/MyBestBook.cs
+++ Assets/CSB_Alpha/SB_Scripts/MyBestBook.cs
@@ -21,7 +21,7 @@ public class MyBestBook : MonoBehaviour
     void Start()
     {
         player = GameObject.Find("Character");
-
+        me.GetComponent<Button>().onClick.AddListener(ShowClickHereBestBook);
     }
 
     void Update()
@@ -65,7 +65,9 @@ public class MyBestBook : MonoBehaviour
 
                 idx = me.transform.GetSiblingIndex();
                 print("³ªÀÇ ÀÎµ¦½º " + idx);
+
                 toggles[idx] = me.GetComponent<Toggle>().isOn;
+               
                 Debug.Log("toggles[idx] = me.GetComponent<Toggle>().isOn;" + toggles[idx] + ":" + me.GetComponent<Toggle>().isOn);
             }
 

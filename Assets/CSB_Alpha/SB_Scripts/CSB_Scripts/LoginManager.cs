36230cc5884ab0020a53931af3355470f5805e74
sbeencho <glitterbeen99@gmail.com>
2022-11-25 10:47:23 +0900

    정리

--- Assets/CSB_Alpha/SB_Scripts/CSB_Scripts/LoginManager.cs
+++ Assets/CSB_Alpha/SB_Scripts/CSB_Scripts/LoginManager.cs
@@ -101,7 +101,7 @@ public class LoginManager : MonoBehaviourPunCallbacks
     {
         base.OnJoinedLobby();
         print(System.Reflection.MethodBase.GetCurrentMethod().Name);
-        PhotonNetwork.LoadLevel("MyRoomScene_Beta");
+        PhotonNetwork.LoadLevel("MyRoomScene_Beta_CSB");
     }
     #endregion
 
